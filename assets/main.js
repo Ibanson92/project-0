fc7a8cf7eee65510bb86bebe9dceba981f472c61
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-05-22 14:56:11 -0400

    result pages

--- assets/main.js
+++ assets/main.js
@@ -9,13 +9,18 @@ value4 = 0;
 console.log(value4);
 valueTotal = 0;
 console.log(valueTotal);
+valuex = 0;
+console.log(valuex);
 
 $(document).ready(function() {
 
 
 $('input:radio').on("click", function(){
-    value1 = $("form input[name='q1']:checked").val();
+    
+    value1 = $("form input[class='other']:checked").val();
+    valuex = $("form input[id='random']:checked").val() + (Math.floor(Math.random() * (4-1)+1));
     console.log("value1=" + value1);
+    console.log("valuex=" + valuex);
     }); //end input:radio.change function
 
     $('input:radio').on("click", function(){
@@ -35,55 +40,47 @@ $('input:radio').on("click", function(){
 
 $('#submit').on("click", function() {
     valueTotal = value1 + value2 + value3 + value4;
+    valueTotalx = valuex + value2 + value3 + value4;
     console.log(valueTotal);
 
-    if (valueTotal == 1111 || valueTotal == 1121 || valueTotal == 1131 || valueTotal == 1141) {
-
+    if (valueTotal == 1111 || valueTotalx == 1111) {
+        location.href = "restaurants/cheapitalian.html";
     };
-    if (valueTotal == 1211 || valueTotal == 1221 || valueTotal == 1231 || valueTotal == 1241) {
-
+    if (valueTotal == 1121 || valueTotalx == 1121) {
+        location.href = "restaurants/cheapseafood.html";
     };
-    if (valueTotal == 1311 || valueTotal == 1321 || valueTotal == 1331 || valueTotal == 1341) {
-
+    if (valueTotal == 1131 || valueTotalx == 1131) {
+        location.href = "restaurants/cheapmeat.html";
     }; 
-    if (valueTotal == 1411 || valueTotal == 1421 || valueTotal == 1431 || valueTotal == 1441) {
-        location.href = 'http://mcdonalds.com';
+    if (valueTotal == 1141 || valueTotalx == 1141) {
+        location.href = "restaurants/cheapvegan.html";
     };
-    if (valueTotal == 2111 || valueTotal == 2121 || valueTotal == 2131 || valueTotal == 2141) {
-
+    if (valueTotal == 2111 || valueTotalx == 2111) {
+        location.href = "restaurants/lowpriceitalian.html";
     }; 
-    if (valueTotal == 2211 || valueTotal == 2221 || valueTotal == 2231 || valueTotal == 2241) {
-
+    if (valueTotal == 2121 || valueTotalx == 2121) {
+        location.href = "restaurants/lowpricesea.html";
     };
-    if (valueTotal == 2311 || valueTotal == 2321 || valueTotal == 2331 || valueTotal == 2341) {
-
+    if (valueTotal == 2131 || valueTotalx == 2131) {
+        location.href = "restaurants/lowpricemeat.html";
     };
-    if (valueTotal == 2411 || valueTotal == 2421 || valueTotal == 2431 || valueTotal == 2441) {
-        location.href = 'http://starbucks.com';
+    if (valueTotal == 2141 || valueTotalx == 2141) {
+        location.href = "restaurants/lowpricevegan.html";
     };
-    if (valueTotal == 3111 || valueTotal == 3121 || valueTotal == 3131 || valueTotal == 3141) {
-
+    if (valueTotal == 3111 || valueTotalx == 3111) {
+        location.href = "restaurants/highpriceitalian.html";
     };
-    if (valueTotal == 3211 || valueTotal == 3221 || valueTotal == 3231 || valueTotal == 3241) {
-
+    if (valueTotal == 3121 || valueTotalx == 3121) {
+        location.href = "restaurants/highpricesea.html";
     };
-    if (valueTotal == 3311 || valueTotal == 3321 || valueTotal == 3331 || valueTotal == 3341) {
-
+    if (valueTotal == 3131 || valueTotalx == 3131) {
+        location.href = "restaurants/highpricemeat.html";
     };
-    if (valueTotal == 3411 || valueTotal == 3421 || valueTotal == 3431 || valueTotal == 3441) {
-        location.href = 'http://kfc.com';
+    if (valueTotal == 3141 || valueTotalx == 3141) {
+        location.href = "restaurants/highpricevegan.html";
     };
-    if (valueTotal == 4111 || valueTotal == 4121 || valueTotal == 4131 || valueTotal == 4141) {
-
-    };
-    if (valueTotal == 4211 || valueTotal == 4221 || valueTotal == 4231 || valueTotal == 4241) {
-
-    };
-    if (valueTotal == 4311 || valueTotal == 4321 || valueTotal == 4331 || valueTotal == 4341) {
-
-    };
-    if (valueTotal == 4411 || valueTotal == 4421 || valueTotal == 4431 || valueTotal == 4441) {
-        location.href = 'http://google.com';
+    if (valueTotal == 1112 || valueTotalx == 1112 || valueTotal == 2112 || valueTotalx == 2112 || valueTotal == 3112 || valueTotalx == 3112 || valueTotal == 1122 || valueTotalx == 1122 || valueTotal == 1132 || valueTotalx == 1132 || valueTotal == 1142 || valueTotalx == 1142 || valueTotal == 2112 || valueTotalx == 2112 || valueTotal == 2122 || valueTotalx == 2122 || valueTotal == 2132 || valueTotalx == 2132 || valueTotal == 2142 || valueTotalx == 2142 || valueTotal == 3112 || valueTotalx == 3112 || valueTotal == 3122 || valueTotalx == 3122 || valueTotal == 3132 || valueTotalx == 3132 || valueTotal == 3142 || valueTotalx == 3142) {
+        location.href = "party.html";
     };
 
 }) //end submit.on click function
